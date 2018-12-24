ee178b967f95a01f1a671ea8134a635a224ffd8e
Rashid <rashid.batukaev@gmail.com>
2018-12-24 23:29:53 +0300

    Update mado.js

--- js/mado.js
+++ js/mado.js
@@ -1,19 +1,43 @@
 $(document).ready(function(){
+
 	var imageCollection = ['uploads/slider/11.jpg','uploads/slider/22.jpg',
-	'uploads/slider/33.jpg','uploads/slider/44.jpg','uploads/slider/55.jpg',
-	'uploads/slider/66.jpg','uploads/slider/77.jpg'];
+	'uploads/slider/33.jpg','uploads/slider/44.jpg','uploads/slider/55.jpg'];
 
-	var current = 1;
+	var mainContent = ['Всероссийское совещание руководителей системы ДПО',
+	'Всероссийский семинар - совещание "Система оценки качества образования:проблемы и пути решения'
+,'Семинар для руководителей общеобразовательных организаций-членов Ассоциации директоров ЧР',
+'Семинар по вопросам проектирования современного урока и его анализа',
+'Семинар на тему «Методика формирования деятельностных уроков истории и обществознания»'];
+
 	var index = 0;
-	setInterval(function(){
-	 var image = $('slide-main-content');
-	 $('.slide-main-content').css('background-image',
-			'url('+imageCollection[index])+')'
-	 index++;
-	if(index>=imageCollection.length){
-	    index = 0;
-	}
-	},4000);
-	
 
+	for (var i = 0; i < imageCollection.length; i++) {
+		$('.dot-content').append('<div class="dot"></div>');
+		if (i == 0) {
+			$('.dot').addClass('active')
+		}
+	}
+
+	setInterval(function(){
+
+		var dots = document.getElementsByClassName('dot');
+
+		for(var i = index; i < dots.length; i++) {
+			$('.dot').eq(index).addClass('active');
+			$('.dot').eq(index-1).removeClass('active');
+		}
+
+		$('.slide-main-content').css({
+		'background-image':'url('+imageCollection[index]+')'
+		});
+
+		index ++;
+
+		if (index >= imageCollection.length) {
+			index = 0;
+		}
+
+	},2000)
+
+	
 });
